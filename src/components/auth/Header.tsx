e93b1243a7a765840ce6dfc36dd2539ee6a1f6de
Jireh Battung <jirehgbattung@gmail.com>
2023-07-25 19:11:55 +0800

    add header and apply logout dispatch

---
+++ src/components/auth/Header.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { auth } from '../../firebase';
+import { Link, useHistory } from 'react-router-dom';
+import { RootState } from '../../store';
+import { signOut } from 'firebase/auth';
+import { logoutAction } from '../../store/actions/authActions';
+
+const Header: React.FC = () => {
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+  
+  const history = useHistory ();
+  const dispatch = useDispatch();
+
+  const handleLogout: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
+    try {
+      await signOut(auth)
+      dispatch(logoutAction());
+      history.push('/');
+    } catch (error: any) {
+      console.log('signout error')
+    }
+  }
+
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="max-w-7xl mx-auto px-4">
+        <ul className="flex items-center justify-between">
+          <li>
+            <Link to="/" className="text-white font-semibold text-lg">
+              Home
+            </Link>
+          </li>
+          <li className="space-x-4">
+            {isLoggedIn ? (
+              <>
+                {/* <Link to="/profile" className="text-white font-medium">
+                  Profile
+                </Link>
+                <Link to="/favorites" className="text-white font-medium">
+                  Favorites
+                </Link> */}
+                <button
+                  onClick={handleLogout}
+                  className="text-white font-medium border border-white rounded-md px-3 py-1"
+                >
+                  Logout
+                </button>
+              </>
+            ) : (
+              <Link to="/authentication" className="text-white font-medium">
+                Login
+              </Link>
+            )}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
