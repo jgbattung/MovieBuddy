61178e1bb83d2585af5b2935cf5e08b1478184b9
Jireh Battung <jirehgbattung@gmail.com>
2023-07-25 19:12:39 +0800

    update components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import IPage from '../interfaces/page'
+import Header from '../components/auth/Header'
 
 const HomePage:React.FunctionComponent<IPage> = props => {
   return (
-    <div>MOVIEBUDDY</div>
+    <div>
+      <Header />
+      <h1>MOVIEBUDDY</h1>
+    </div>
   )
 }
 
