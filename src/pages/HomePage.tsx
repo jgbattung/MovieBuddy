8fa692c181d0d9d6fae269fe2c624e3ec80bd909
Jireh Battung <jirehgbattung@gmail.com>
2023-07-31 16:56:54 +0800

    update registration page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import IPage from '../interfaces/page'
+import Header from '../components/common/Header'
+import Home from '../components/auth/Home'
 
 const HomePage:React.FunctionComponent<IPage> = props => {
   return (
-    <div>MOVIEBUDDY</div>
+    <div>
+      <Header />
+      <Home />
+    </div>
   )
 }
 
