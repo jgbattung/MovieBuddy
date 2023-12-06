6862b8efc85324f034e5ed57551ec72e6e3e151c
Jireh Battung <jirehgbattung@gmail.com>
2023-12-06 17:00:49 +0800

    fix favorites link

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -35,11 +35,21 @@ const Header: React.FC = () => {
       <p className="self-center text-3xl font-semibold whitespace-nowrap text-white">Movie<span className='text-3xl font-bold text-indigo-600'>Buddy</span></p>
       <div className="flex md:order-2">
         {isLoggedIn ? (
-          <button
-            onClick={handleLogout}
-            className="text-white text-md bg-indigo-600 transition hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2 text-center mr-3 md:mr-0">
-              Logout
-          </button>
+          <>
+            <div className='font-medium p-4 md:p-0 mt-4 md:flex-row md:mt-0 md:border-0 hover:text-indigo-400 transition-all'>
+              <Link to="/favorites" className="flex items-center justify-center py-2 pr-20 text-white hover:text-indigo-400 transition-all">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6 mr-2">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                </svg>
+                Favorites
+              </Link>
+            </div>
+            <button
+              onClick={handleLogout}
+              className="text-white text-md bg-indigo-600 transition hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2 text-center mr-3 md:mr-0">
+                Logout
+            </button>
+          </>
         ) : (
           <button
             onClick={handleLogin}
@@ -53,9 +63,6 @@ const Header: React.FC = () => {
             <>
               <li>
                 <Link to="/homepage" className="block py-2 pl-3 pr-4 mx-8 text-white hover:text-indigo-400">Home</Link>
-              </li>
-              <li>
-                <Link to="/favorites" className="block py-2 pl-3 pr-4 mx-8 text-white hover:text-indigo-400">Favorites</Link>
               </li>
             </>
           ) : (
