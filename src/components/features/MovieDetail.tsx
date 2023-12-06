046a6eb85e6fbbcbe6abfc741f6deb2a74f5edd4
Jireh Battung <jirehgbattung@gmail.com>
2023-12-06 17:37:24 +0800

    add error handling

--- src/components/features/MovieDetail.tsx
+++ src/components/features/MovieDetail.tsx
@@ -278,12 +278,12 @@ const MovieDetail: React.FC = () => {
               <p className="font-extrabold text-3xl text-indigo-800">|</p>
               <p className="font-bold text-2xl pl-3">Did you know?</p>
             </div>
-            {movieTrivia?.unspoilt.slice(0,5).map((trivia: ITriviaUnspoilt) => (
+            {movieTrivia?.unspoilt?.slice(0,5).map((trivia: ITriviaUnspoilt) => (
               <div className="bg-slate-200 max-w-2xl overflow-hidden shadow-lg rounded-md mx-5 my-4 px-8 py-5">
                 <p className='text-gray-700'>{trivia.text}</p>
               </div>
             ))}
-            {movieTrivia?.spoilt.slice(0, 5).map((trivia: ITriviaSpoilt, index) => (
+            {movieTrivia?.spoilt?.slice(0, 5).map((trivia: ITriviaSpoilt, index) => (
               <div
                 className="mx-5 my-4 max-w-2xl overflow-hidden rounded-md bg-slate-200 px-8 py-5 shadow-lg relative group"
                 key={index}
@@ -313,7 +313,7 @@ const MovieDetail: React.FC = () => {
                       <figure><img src={similarFilm.title.image.url} alt={similarFilm.title.title}/></figure>
                       <div className='flex items-center pt-2'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 24 24" strokeWidth="0.3" stroke="gray" className="h-7 w-7 pl-1">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                         </svg>
                         <p className='text-gray-500 font-light pl-1'>{similarFilm.ratings.rating}</p>
                       </div>
