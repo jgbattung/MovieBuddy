421ca638abf75732d58abd58d782bdca24208c07
Jireh Battung <jirehgbattung@gmail.com>
2023-12-07 17:28:40 +0800

    fix logout on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
 import routes from './config/routes';
 import './App.css'
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+import { loginAction } from './store/actions/authActions';
+import { fetchAndSetUserData } from './utils/firebaseFunctions';
+import { setUserData } from './store/actions/userActions';
+import { IUserData } from './interfaces/userData';
 
 const App: React.FunctionComponent = () => {
+  const history = useHistory ();
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        dispatch(loginAction());
+        
+        const userData = await fetchAndSetUserData()
+        if (userData) {
+          dispatch(setUserData(userData as IUserData));
+          console.log('Success');
+        }
+
+        if (history && history.location.pathname !== '/homepage') {
+          history.replace(history.location.pathname);
+        }
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [history]);
+
+
   return (
     <BrowserRouter>
       <div className="App">
--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { auth } from '../../firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { getErrorMessage } from '../../utils';
